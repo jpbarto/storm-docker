2d61067c36dc4d27a4cbd308a2377b915b127850
Jason Barto <jason.p.barto@lmco.com>
2016-06-10 12:29:15 +0100

    removed unnecessary port definition from script defaults

--- storm-nimbus/start-supervisor.sh
+++ storm-nimbus/start-supervisor.sh
@@ -1,6 +1,6 @@
 export IP=`hostname -i | cut -d ' ' -f1`
 # provide this env var with a default in case its unset
-${ZK_PORT_2181_TCP_ADDR:=zk:2181}
+${ZK_PORT_2181_TCP_ADDR:=zk}
 sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$IP/g" $STORM_HOME/conf/storm.yaml
 
